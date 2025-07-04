72cd03eb3b7a681cf6e0bc0652de832e4f731387
vagabond <vagabond.git@gmail.com>
2025-07-04 14:26:43 +0200

    fix: update css for forms and fix avatar and toast component

--- projects/ng-dsv/ds/avatar/component/avatar.component.stories.ts
+++ projects/ng-dsv/ds/avatar/component/avatar.component.stories.ts
@@ -25,6 +25,23 @@ export default meta;
 type Story = StoryObj<DsvAvatarComponent>;
 
 export const Default: Story = {
+  args: {
+    callback: () => { },
+  },
+  render: (args: any) => ({
+    props: {
+      callback: args.callback,
+    },
+    template: `
+      <div class="flex flex-row gap10">
+        <dsv-avatar avatar="${args.avatar}" color="${args.color}" variant="${args.variant}" width="${args.width}" show="${args.show}" show="${args.show}"></dsv-avatar>
+        <dsv-avatar avatar="${args.avatar}" color="${args.color}" variant="${args.variant}" width="${args.width}" show="${args.show}" show="${args.show}" (callback)="callback"></dsv-avatar>
+      </div>
+    `,
+  }),
+};
+
+export const NoCallback: Story = {
   render: (args: any) => ({
     template: `<dsv-avatar avatar="${args.avatar}" color="${args.color}" variant="${args.variant}" width="${args.width}" show="${args.show}" show="${args.show}"></dsv-avatar>`,
   }),
