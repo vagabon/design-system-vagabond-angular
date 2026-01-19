ed604998a4c0be3c9aaae2d54a061ea8ee7faf14
vagabond <vagabond.git@gmail.com>
2026-01-19 11:04:18 +0100

    fix(input): input signal without elementRef

--- projects/ng-dsv/ds/form/signal/input/component/form.signal.input.component.ts
+++ projects/ng-dsv/ds/form/signal/input/component/form.signal.input.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component, input } from '@angular/core';
+import { Component, input } from '@angular/core';
 import { FormField } from '@angular/forms/signals';
 import { INPUT_TYPE } from '@ng-vagabond-lab/ng-dsv/type';
 import { FormSignalInputBase } from '../../base/form.signal.input.base';
@@ -15,17 +15,9 @@ import { FormSignalLabelComponent } from '../../label/component/form.signal.labe
   templateUrl: './form.signal.input.component.html',
   styleUrl: '../../../reactive/input/component/form.reactive.input.component.scss',
 })
-export class FormSignalInputComponent<T> extends FormSignalInputBase<T> implements AfterViewInit {
-  // @ViewChild('input')
-  // input!: ElementRef<HTMLInputElement>;
+export class FormSignalInputComponent<T> extends FormSignalInputBase<T> {
 
   type = input<INPUT_TYPE>('text');
   icon = input<string>();
-
-  ngAfterViewInit() {
-    // if (this.input?.nativeElement) {
-    //   this.input.nativeElement.type = this.type();
-    // }
-  }
 
 }
