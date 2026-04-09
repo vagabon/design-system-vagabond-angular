3f52254ce7c7420790e3cd1074e5c26bf82e27d1
vagabond <vagabond.git@gmail.com>
2026-04-09 05:32:40 +0200

    chore: update versions

--- projects/ng-dsv/storage/store/store.ts
+++ projects/ng-dsv/storage/store/store.ts
@@ -13,6 +13,13 @@ export class Store<T> {
         });
     }
 
+    updateTable(id: number, data: T) {
+        this.data.update((values) => {
+            values[id] = [...data as T[]] as T;
+            return [...values];
+        });
+    }
+
     updateForPage(page: number, data: T[]) {
         this.data.update((values) => {
             let newValues = page === 1 ? [] : [...values];
