163d0a2572843f319288d18d91895fdbcf96260a
vagabond <vagabond.git@gmail.com>
2026-04-28 14:44:49 +0200

    feat(router): update service to have an interval that can be clean when route changes

--- projects/ng-dsv/router/service/router.service.ts
+++ projects/ng-dsv/router/service/router.service.ts
@@ -1,6 +1,6 @@
-import { effect, inject, Injectable } from '@angular/core';
+import { effect, inject, Injectable, signal } from '@angular/core';
 import { toSignal } from '@angular/core/rxjs-interop';
-import { NavigationEnd, Router } from '@angular/router';
+import { NavigationCancel, NavigationEnd, NavigationError, NavigationStart, Router } from '@angular/router';
 import { ModalService } from '@ng-vagabond-lab/ng-dsv/ds/modal';
 import { filter, map } from 'rxjs';
 
@@ -11,6 +11,10 @@ export class RouterService {
     readonly router = inject(Router);
     readonly modalService = inject(ModalService);
 
+    readonly isLoading = signal<boolean>(true);
+
+    interval: ReturnType<typeof setTimeout> | undefined = undefined;
+
     currentUrl = toSignal(
         this.router.events.pipe(
             filter((event) => event instanceof NavigationEnd),
@@ -20,6 +24,19 @@ export class RouterService {
     );
 
     constructor() {
+        this.router.events.subscribe((event) => {
+            if (event instanceof NavigationStart) {
+                this.isLoading.set(true);
+            } else if (
+                event instanceof NavigationEnd ||
+                event instanceof NavigationCancel ||
+                event instanceof NavigationError
+            ) {
+                this.isLoading.set(false);
+                clearInterval(this.interval);
+            }
+        });
+
         effect(() => {
             this.currentUrl();
             this.modalService.closeAll();
