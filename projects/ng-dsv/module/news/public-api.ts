79ed4813269c90e4b5b212f6f4959405ea8ff870
vagabond <vagabond.git@gmail.com>
2026-09-02 23:25:57 +0200

    feat(news): add component to lib

--- projects/ng-dsv/module/news/public-api.ts
+++ projects/ng-dsv/module/news/public-api.ts
@@ -1,2 +1,4 @@
 //export * from './component/card/news-card.component';
+export * from './component/card/resume/news-card-resume.component';
+export * from './dto/news.dto';
 export * from './route/news.route';
